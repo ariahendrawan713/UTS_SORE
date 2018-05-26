8fe08c4d630247adcfdf3401cbc8b6766394430a
rdebika <38754312+rdebika@users.noreply.github.com>
2018-05-27 04:50:26 +0700

    update

--- python1.py
+++ python1.py
@@ -1,17 +1,17 @@
-+var = 10
-+if ( var == 100 ) : print "Value of expression is 100"
-+else : 
-+      print "Value of expression is not 100"
-+      print "Good bye!"
-+
-+
-+a = 33
-+b = 200
-+if ( b > a ):
-+    print "b is greater than a"
+var = 10
+if ( var == 100 ) : print "Value of expression is 100"
+else :
+    print "Value of expression is not 100"
+    print "Good bye"
+
+
+a = 33
+b = 200
+if ( b > a ):
+    print "b is greater than a"
 
 
 
-+mahasiswa = ["jangan", "lupa", "ucapkan", "salam"]
-+for x in mahasiswa:
-+   print(x)
+mahasiswa = ["jangan", "lupa", "ucapkan", "salam"]
+for x in mahasiswa:
+    print x
