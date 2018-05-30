e1296096aad5444f04e2def4bb128a5346ce44e8
slametfatkhuli <38583632+slametfatkhuli@users.noreply.github.com>
2018-05-30 20:25:02 +0700

    SlametFatkhuli_G231150071_UTS_OSS_SORE.txt

--- python1.py
+++ python1.py
@@ -1,13 +1,19 @@
 var = 10
-if ( var == 100 ) : print "Value of expression is 100"
-else (var != 100): print "Value of expression is not 100"
+if var == 100 :
+    print "Value of expression is 100"
+    print var
+else :
+    print "Value of expression is not 100"
+    print var
 print "Good bye!"
 
 a = 33
 b = 200
-if b > a
-print("b is greater than a") % you will get an error
-
-mahasiswa = ["jangan", "lupa", "ucapkan", "salam"]
+if b > a:
+    print "b is greater than a" #% you will get an error
+else :
+    print "b is lower than a"
+    
+mahasiswa = ["jangan", "lupa", "ucapkan", "salam" , "Assalamualaikum"]
 for x in mahasiswa:
   print(x)
